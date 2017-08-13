6a2e072a9056b01ddad3a744d97290c1d0633b15
Vidushan Kohiladas <vidushan@Vidushans-MacBook-Air.local>
2017-08-13 15:04:00 -0700

    gets all live bus data, along with creating a database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
+"use strict";
+require('dotenv').config();
 const express = require('express');
+const bodyParser  = require("body-parser");
+const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.NODE_ENV || 'development';
+const knexConfig  = require("../knexfile.js");
+const knex = require("knex")(knexConfig[ENV]);
+const api = require('../routes/translink');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('build'));
+app.use('/getbusjson', api);
+
 app.listen(3000, () => {
   console.log(`Server listening on port ${PORT} in ${ENV} mode.`);
 });
